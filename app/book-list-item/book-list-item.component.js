dc90d0078b1a76348b57d79af26dd48c0f3e6d5a
The Buildbot <buildbot@angular2buch.de>
2016-04-20 15:40:24 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/Angular2Buch/book-monkey2/commit/b64373a6374940e02f8c6fdf0c017adb8fa670c8
    Travis build: https://travis-ci.org/Angular2Buch/book-monkey2/builds/124503458

--- app/book-list-item/book-list-item.component.js
+++ app/book-list-item/book-list-item.component.js
@@ -9,7 +9,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('angular2/core');
-var book_1 = require('../../domain/book');
+var book_1 = require('../domain/book');
 var isbn_pipe_1 = require('../pipes/isbn-pipe/isbn-pipe');
 var BookListItem = (function () {
     function BookListItem() {
